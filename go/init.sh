2ae06a3fd645385a0af9fb99d83d424996bdceb3
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-04-07 23:40:33 +0700

    add aliases for maim and go

--- go/init.sh
+++ go/init.sh
@@ -1,1 +1,4 @@
+export GOPATH="$(cat ~/Applications/Config/Go/GOPATH.md)"
+
 alias gqlgen="go run github.com/99designs/gqlgen"
+alias GoSrc='ranger "/media/My_Drive/Kelas Terbuka/Projects/Golang/src"'
