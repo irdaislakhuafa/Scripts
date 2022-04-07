23f2664f143931d8771ca50696401df44ba4bf7f
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-04-07 23:41:29 +0700

    add custom scripts for alterra

---
+++ alterra/init.sh
@@ -0,0 +1,5 @@
+alias createSection="mkdir -v praktikum  screenshots && touch summary.md && echo 'Success created section on this directory'"
+alias createSectionVim="mkdir -v praktikum screenshots && vim summary.md && echo 'Success created section on this directory'"
+alias altaClass='ranger "/media/My_Drive/Kelas Terbuka/Projects/java-springboot_irda-islakhu-afa"'
+alias alterra='ranger "/media/My_Drive/Kelas Terbuka/Altera Academy"'
+alias openAltaClass="cd '/media/My_Drive/Kelas Terbuka/Projects/java-springboot_irda-islakhu-afa' && code ."
