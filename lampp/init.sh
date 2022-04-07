1dd32c6e69273db4ab42982dd29a40c62555cbfb
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-04-08 00:19:38 +0700

    add start stop and status

--- lampp/init.sh
+++ lampp/init.sh
@@ -1,3 +1,7 @@
 export PATH=/opt/lampp/bin:$PATH
 export PATH=/opt/lampp:$PATH
+
 alias lampp='doas lampp'
+alias lstart='doas lampp start'
+alias lstop='doas lampp stop'
+alias lstatus='doas lampp status'
