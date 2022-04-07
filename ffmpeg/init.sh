4082a51ffb55681f49d38cf8a5657747248b7e35
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-04-08 00:14:57 +0700

    add custom command for ffmpeg and add edit pacman config on nvim

---
+++ ffmpeg/init.sh
@@ -0,0 +1,2 @@
+# edit record screen file directory
+alias drecord='nvim /home/artix/.config/i3/scripts/ffmpeg/directory'
--- nvim/init.sh
+++ nvim/init.sh
@@ -1,2 +1,5 @@
 alias eacc='nvim /media/My_Drive/Kelas\ Terbuka/Account.Me'
 alias etri='nvim ~/.config/trizen/trizen.conf'
+
+# edit pacman config
+alias epac='edit /etc/pacman.conf'
