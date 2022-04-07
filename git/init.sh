3504737975651b0d173d9963f1c42c93f10983b1
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-04-08 00:08:24 +0700

    add custom command for git

---
+++ git/init.sh
@@ -0,0 +1,18 @@
+alias grph='git log \
+--all \
+--decorate \
+--oneline \
+--graph'
+
+alias gadd='git add'
+alias gout='git checkout'
+alias gmit='git commit'
+alias gmrg='git merge'
+alias gmrgff='gmrg --ff'
+alias gmrgnff='gmrg --no-ff'
+alias gbr='git branch'
+alias gps='git push'
+alias gam='git add . && gmit -am'
+alias gdif='git diff'
+alias gbra='gbr -a'
+alias gamit='gadd . && gmit'
