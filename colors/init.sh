8ce8bc7fe392f29fd7d92665eb9116a63aa34aee
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-04-08 00:16:08 +0700

    add custom command for colors

---
+++ colors/init.sh
@@ -0,0 +1,23 @@
+# foreground
+blue="$fg[blue]"
+red="$fg[red]"
+yellow="$fg[yellow]"
+green="$fg[green]"
+
+# simple foreground color
+B="$blue"
+R="$red"
+Y="$yellow"
+G="$green"
+
+# background
+bblue="$bg[blue]"
+bred="$bg[red]"
+byellow="$bg[yellow]"
+bgreen="$bg[green]"
+
+# simple foreground color
+bB="$blue"
+bR="$red"
+bY="$yellow"
+bG="$green"
