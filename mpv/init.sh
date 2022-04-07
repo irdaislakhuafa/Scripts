bcf1003f2dc909a30b01114608dc83169bc52196
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-04-08 00:16:23 +0700

    add custom command for mpv

---
+++ mpv/init.sh
@@ -0,0 +1,17 @@
+alias webcam='mpv /dev/video1 \
+--no-cache \
+--no-demuxer-thread \
+--video-sync=audio \
+--vd-lavc-threads=1 \
+--no-audio \
+--window-scale=0.5 \
+--vf=hflip \
+--profile=low-latency \
+--untimed \
+--opengl-glfinish=yes \
+--fps=60 \
+--framedrop=no \
+--speed=1.01 \
+--opengl-swapinterval=0 \
+--no-correct-pts '
+alias wcam='webcam'
