b4070b17f46be00f0113def240442f7dabb4cd2e
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-04-22 23:42:30 +0700

    add alias to edit i3 config

--- i3/init.sh
+++ i3/init.sh
@@ -1,3 +1,4 @@
 # mode GPU
 alias gmode="if [ $(cat ~/.config/i3/mode) = 1 ]; then echo 'nvidia'; else echo 'intel'; fi"
 alias egmode='nvim ~/.config/i3/mode'
+alias i3bl="nvim ~/.config/i3blocks/config"
