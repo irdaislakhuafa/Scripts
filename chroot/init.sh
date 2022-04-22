75015fb1e2628c3498c319eda97d9c9b5ca60eb0
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-04-22 23:45:40 +0700

    add alias for show nvme health and add arch shortcut

--- chroot/init.sh
+++ chroot/init.sh
@@ -1,1 +1,2 @@
 alias archroot='doas arch-chroot'
+alias arch='doas mount /dev/nvme0n1p3 /media/ArchLinux && doas arch-chroot /media/ArchLinux'
---
+++ smartmontools/init.sh
@@ -0,0 +1,1 @@
+alias nvme-health="doas smartctl -A /dev/nvme0n1"
