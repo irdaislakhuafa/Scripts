2f44e012ec8b6648e3213909a58a9d32153d686d
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-04-22 23:49:34 +0700

    add alias for iwd

---
+++ iwd/init.sh
@@ -0,0 +1,12 @@
+getDevice="$(iwctl device list | grep station | awk '{print$1}')"
+alias wscan='iwctl station $getDevice scan'
+alias dlist='iwctl device list'
+alias wlist='wscan && iwctl station $getDevice get-networks'
+alias wlist-h='wscan && iwctl station $getDevice get-hidden-access-points'
+alias wconnect='iwctl station $getDevice connect'
+alias wconnect-h='iwctl station $getDevice connect-hidden'
+alias wshow='iwctl station $getDevice show'
+alias rwd='rsm restart iwd'
+alias wdisconnect='iwctl station $getDevice disconnect'
+alias won="doas wifi on"
+alias wof="doas wifi off"
