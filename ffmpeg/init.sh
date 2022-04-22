c9f60a48cc0916019f086b521fe521aa7fcf135f
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-04-22 23:47:32 +0700

    add ffmpeg alias to record schreen

--- ffmpeg/init.sh
+++ ffmpeg/init.sh
@@ -1,2 +1,3 @@
 # edit record screen file directory
 alias drecord='nvim /home/artix/.config/i3/scripts/ffmpeg/directory'
+alias rstf="ffmpeg -video_size $(xrandr | grep '*' | awk '{print$1}') -framerate 25 -f x11grab -i $DISPLAY"
