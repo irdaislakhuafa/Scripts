37bf869baaabd9b5eb9deb09bf70049cefd920c9
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-04-22 23:43:30 +0700

    add alias for spring boot

---
+++ gradle/init.sh
@@ -0,0 +1,1 @@
+alias gsrun="gradle bootRun"
--- maven/init.sh
+++ maven/init.sh
@@ -3,3 +3,5 @@ alias mdtree='mvn dependency:tree'
 alias mresolv='mvn dependency:resolve'
 alias mpack='mvn package'
 alias mtest='mvn test'
+alias srun="mvn spring-boot:run"
+alias csrun="mvn clean && srun"
