e34b4e3d8919e94ba0ebf02c62a30fdb7355936c
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-04-22 23:46:25 +0700

    add alias to class

--- ranger/init.sh
+++ ranger/init.sh
@@ -2,3 +2,9 @@
 alias removable='ranger /run/media/artix'
 # go to mtp devices
 alias media='ranger /media/My_Media'
+
+alias kelas="ranger /media/My_Drive/Kelas\ Terbuka/"
+alias data="ranger /media/My_Drive"
+alias projects="ranger /media/My_Drive/Kelas\ Terbuka/Projects"
+alias home="ranger ~/"
+alias this='ranger .'
