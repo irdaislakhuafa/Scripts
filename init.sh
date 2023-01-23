952354304bbf228ad0dd0b537b3708686788c0c7
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-01-23 23:51:32 +0700

    use home user

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
 # my custom variables
-MY_HOME="/home/artix"
-MY_SCRIPTS="$MY_HOME/Scripts"
+MY_SCRIPTS="$HOME/Scripts"
 
 for directory in $MY_SCRIPTS/*; do source $directory/init.sh 2> ~/.cache/not_dir; done
