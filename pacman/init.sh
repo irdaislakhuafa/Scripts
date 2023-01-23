dd3b6edcd95124d40f56149f2cc659fc16410f03
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-01-23 23:45:56 +0700

    remove unused code and mov init aliases to runit/init.sh

---
+++ pacman/init.sh
@@ -0,0 +1,7 @@
+alias pac='doas pacman'
+alias t='trizen'
+alias p='pac'
+alias :v='t'
+alias fix='srm /var/lib/pacman/db.lck'
+alias rmp='p -Rcnsuv'
+alias vcp='doas downgrade'
--- runit/init.sh
+++ runit/init.sh
@@ -9,3 +9,19 @@ alias start='rsm start'
 alias stop='rsm stop'
 alias up='sv up'
 alias down='sv down'
+
+# to bios without presses F2 key
+init="$(pstree -p | grep '(1)' | cut -d '(' -f 1)"
+if [ "$init" = "runit" ]; then
+	alias BIOS='loginctl reboot --firmware-setup'
+elif [ "$init" = "systemd" ]; then
+	alias BIOS="systemctl reboot --firmware-setup"
+fi
+alias bios='BIOS'
+
+# control power
+alias off='doas poweroff'
+alias offf='off -f'
+alias boot='doas reboot'
+alias suspend='doas loginctl suspend'
+
