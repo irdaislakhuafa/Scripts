f87cd5c25451f28d145d8279c48f1c242572e505
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-02-13 19:02:14 +0700

    remove unused code, move pfetch and tasks to init.sh and add env variable to get current gqlgen version

--- go/init.sh
+++ go/init.sh
@@ -1,9 +1,10 @@
-export GQLGEN="@v0.17.13"
-alias gqlgen="go get github.com/99designs/gqlgen && go run github.com/99designs/gqlgen"
-#alias GoSrc='ranger "/media/My_Drive/Kelas Terbuka/Projects/Golang/src"'
-alias gogen='gqlgen generate'
-alias gen="go get github.com/99designs/gqlgen$GQLGEN && go run github.com/99designs/gqlgen$GQLGEN generate"
-alias grun='go run'
-alias ggen="go run github.com/99designs/gqlgen$GQLGEN"
-
+export GOME="github.com/irdaislakhuafa"
 export PATH="/home/artix/go/bin:$PATH"
+
+# get current graphql version
+gql="$(cat go.mod | grep gqlgen)"
+version=$(echo "$gql" | cut -d " " -f 2)
+gqlgen="github.com/99designs/gqlgen@$version"
+
+alias grun='go run'
+alias gqlgen="go run $gqlgen"
--- init.sh
+++ init.sh
@@ -2,3 +2,9 @@
 MY_SCRIPTS="$HOME/Scripts"
 
 for directory in $MY_SCRIPTS/*; do source $directory/init.sh 2> ~/.cache/not_dir; done
+
+# pfetch
+pfetch
+
+# tasks
+task 2> /home/artix/.cache/task
