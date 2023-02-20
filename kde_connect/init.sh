252241b1fad07b07b725c47ef31a0d127342f13c
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-02-20 23:06:05 +0700

    add parameter

--- kde_connect/init.sh
+++ kde_connect/init.sh
@@ -2,10 +2,10 @@ alias kcli="kdeconnect-cli"
 
 kclid() {
     deviceID="$(kcli -l | fzf | cut -d ':' -f 2 | awk '{print $1}' )"
-    kcli -d $deviceID
+    kcli -d $deviceID $@
 }
 
 kclida() {
     deviceID="$(kcli -a | fzf | cut -d ':' -f 2 | awk '{print $1}' )"
-    kcli -d $deviceID
+    kcli -d $deviceID $@
 }
