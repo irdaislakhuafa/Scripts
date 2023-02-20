34725ab3f74a44b1e2e9c5ff67746d488624fa18
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-02-20 22:57:54 +0700

    add func to get device id with kde connect cli

--- kde_connect/init.sh
+++ kde_connect/init.sh
@@ -1,1 +1,11 @@
 alias kcli="kdeconnect-cli"
+
+kclid() {
+    deviceID="$(kcli -l | fzf | cut -d ':' -f 2 | awk '{print $1}' )"
+    kcli -d $deviceID
+}
+
+kclida() {
+    deviceID="$(kcli -a | fzf | cut -d ':' -f 2 | awk '{print $1}' )"
+    kcli -d $deviceID
+}
