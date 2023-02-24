f9342e1b53e31fe989be4d27634f2809311eed8f
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-02-24 15:08:46 +0700

    add function to make git stash apply more easy

--- git/init.sh
+++ git/init.sh
@@ -24,6 +24,16 @@ alias gmed='git commit --amend'
 alias gfap='git fetch --all --prune'
 alias gpl='git pull'
 alias gsdrop='git stash && git stash drop stash@\{0\}'
+alias gslect='git stash list | fzf --reverse | cut -d ":" -f 1'
+gsply() {
+	selected="$(gslect)"
+	if [ "$selected" = "" ]; then
+		echo "Nothing do"
+	else
+		git stash apply $selected
+	fi
+}
+
 alias gprsc='gh pr status -c'
 alias gouts='gout $(gbr | fzf)'
 alias gbrd='gbr -D $(gbr | fzf)'
