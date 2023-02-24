aa6b42d9a5713272ad04fe5a7ba53d5ebddff789
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-02-24 15:38:36 +0700

    add function to close github pull request easyly

--- github/init.sh
+++ github/init.sh
@@ -1,9 +1,21 @@
 alias github-token='cat /media/My_Drive/Kelas\ Terbuka/Account.Me | grep _ '
 alias gprchecks='gh pr checks --watch $1'
-
+ghprlect(){
+	selected="$(gh pr list | fzf --reverse | awk '{print $1}')"
+	echo $selected
+}
 ghprout() {
 	list="$(gh pr list)"
 	prID="$(echo $list | fzf | awk '{print $1}')"
 	gh pr checkout $prID
+}
+ghprlosep() {
+	selected=$(ghprlect)
+
+	if [ "$selected" = "" ]; then
+		echo "Nothing do";
+	else
+		gh pr close $selected
+	fi
 }
 
