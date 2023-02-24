2be828628260b71a131d788fc52ef09a5fbe66be
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-02-24 18:16:00 +0700

    add alias to view pull request merged

--- github/init.sh
+++ github/init.sh
@@ -2,7 +2,7 @@ alias github-token='cat /media/My_Drive/Kelas\ Terbuka/Account.Me | grep _ '
 alias gprchecks='gh pr checks --watch $1'
 
 ghprlect(){
-	selected="$(gh pr list | fzf --reverse | awk '{print $1}')"
+	selected="$(gh pr list $@ | fzf --reverse | awk '{print $1}')"
 	echo $selected
 }
 
@@ -15,8 +15,19 @@ ghprx() {
 	fi
 }
 
+ghprmx() {
+	selected=$(ghprlect --state merged --limit 1000)
+	if [ "$selected" = "" ]; then
+		echo "Nothing do";
+	else
+		$@ $selected
+	fi
+}
+
 alias ghprout='ghprx gh pr checkout'
 alias ghprlose='ghprx gh pr close'
 alias ghprdit='ghprx gh pr edit'
 alias ghprv='ghprx gh pr view'
 alias ghprm='ghprx gh pr merge'
+alias ghprvm='ghprmx gh pr view'
+alias ghprvmw='ghprmx gh pr view --web'
