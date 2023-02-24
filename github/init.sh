197a5cafddfa8d2aec3a1b2c91176a3ba258b525
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-02-24 15:48:28 +0700

    make it more simple

--- github/init.sh
+++ github/init.sh
@@ -1,21 +1,20 @@
 alias github-token='cat /media/My_Drive/Kelas\ Terbuka/Account.Me | grep _ '
 alias gprchecks='gh pr checks --watch $1'
+
 ghprlect(){
 	selected="$(gh pr list | fzf --reverse | awk '{print $1}')"
 	echo $selected
 }
-ghprout() {
-	list="$(gh pr list)"
-	prID="$(echo $list | fzf | awk '{print $1}')"
-	gh pr checkout $prID
-}
-ghprlosep() {
-	selected=$(ghprlect)
 
+ghprx() {
+	selected=$(ghprlect)
 	if [ "$selected" = "" ]; then
 		echo "Nothing do";
 	else
-		gh pr close $selected
+		$@ $selected
 	fi
 }
 
+alias ghprout='ghprx gh pr checkout'
+alias ghprlose='ghprx gh pr close'
+alias ghprdit='ghprx gh pr edit'
