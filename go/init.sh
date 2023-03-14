0492ee491cceb31730dd76fefaab8a4fc57fb306
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-03-14 16:13:28 +0700

    generate gql server with gqlge

--- go/init.sh
+++ go/init.sh
@@ -1,10 +1,20 @@
 export GOME="github.com/irdaislakhuafa"
 export PATH="/home/artix/go/bin:$PATH"
 
-# get current graphql version
-gql="$(cat go.mod | grep gqlgen)"
-version=$(echo "$gql" | cut -d " " -f 2)
-gqlgen="github.com/99designs/gqlgen@$version"
+
+gqlgenv() {
+	# get current graphql version
+	gql="$(cat go.mod | grep gqlgen)"
+	version=$(echo "$gql" | cut -d " " -f 2)
+	
+	if [ "$version" = "" ]; then
+		version="latest"
+	fi
+
+	gqlgenversion="github.com/99designs/gqlgen@$version"
+	echo "$gqlgenversion"
+
+}
 
 alias grun='go run'
-alias gqlgen="go run $gqlgen"
+alias gqlgen='go run $(gqlgenv)'
