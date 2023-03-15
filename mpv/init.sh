4502ddb5406386b5d8fd8fa261e859753fd07bfb
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-03-15 13:46:26 +0700

    add indentation

--- mpv/init.sh
+++ mpv/init.sh
@@ -1,19 +1,19 @@
 alias webcam='mpv /dev/video0 \
---no-cache \
---no-demuxer-thread \
---video-sync=audio \
---vd-lavc-threads=1 \
---no-audio \
---window-scale=0.5 \
---vf=hflip \
---profile=low-latency \
---untimed \
---opengl-glfinish=yes \
---fps=60 \
---framedrop=no \
---speed=1.01 \
---opengl-swapinterval=0 \
---no-correct-pts '
+	--no-cache \
+	--no-demuxer-thread \
+	--video-sync=audio \
+	--vd-lavc-threads=1 \
+	--no-audio \
+	--window-scale=0.5 \
+	--vf=hflip \
+	--profile=low-latency \
+	--untimed \
+	--opengl-glfinish=yes \
+	--fps=60 \
+	--framedrop=no \
+	--speed=1.01 \
+	--opengl-swapinterval=0 \
+	--no-correct-pts '
 alias wcam='webcam'
 alias npv="prime-run mpv"
 alias nvpv='mpv --no-vid'
