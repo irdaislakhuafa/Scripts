3cdab1a7347711cb0f94bd8694a116c90cf35ed7
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-04-01 16:20:47 +0700

    add some env variable to use nvidia in wayland/hyprland

--- hyprland/init.sh
+++ hyprland/init.sh
@@ -1,3 +1,14 @@
+# enable hardware acceleration in nvidia GPUs
+export LIBVA_DRIVER_NAME=nvidia
+
+# force use GBM as backend https://wiki.archlinux.org/title/Wayland#Requirements
+export GBM_BACKEND=nvidia-drm
+export __GLX_VENDOR_LIBRARY_NAME=nvidia
+
+# Controls if Adaptive Sync should be used. Recommended to set as “0” to avoid having problems on some
+export __GL_VRR_ALLOWED=0
+
+# use nvidia for wayland session
 export IS_NVIDIA=false
 if [ "$(tty)" = "/dev/tty1" ]; then
 	if [ $IS_NVIDIA ]; then
