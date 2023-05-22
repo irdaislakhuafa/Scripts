3bec00bb4a259cf5abcde825a0d1e7e1350ee9e1
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-05-23 05:05:44 +0700

    add prompt to input password if wifi need password

--- nmcli/init.sh
+++ nmcli/init.sh
@@ -1,20 +1,26 @@
+WIFI_NO_PASSWORD_PROVIDED="Error: Connection activation failed: Secrets were required, but not provided."
 alias nmcli_dev_wifi_list='nmcli dev wifi list'
 alias nmcli_dev_wifi_rescan='nmcli dev wifi rescan'
 
 nmcli_get_ssid() {
-	ssid="$(nmcli_dev_wifi_list | fzf | awk '{print $1}')"
+	ssid="$(nmcli_dev_wifi_list | fzf)"
 	echo "$ssid"
 }
 
 nmcli_dev_wifi_connect() {
-	ssid="$(nmcli_get_ssid)"; 
+	selected="$(nmcli_get_ssid)"
+	ssid="$( echo $selected | awk '{print $1}')"; 
 	if [ "$ssid" = "*" ]; then 
 		echo "Already connected to here."; 
 	elif [ "$ssid" = "" ]; then 
 		echo "Nothing do."; 
 	else 
-		echo $@
-		nmcli dev wifi connect "$ssid" $@
+		result="$(nmcli dev wifi connect $ssid $@ 2>&1)"
+		if [ "$result" = "$WIFI_NO_PASSWORD_PROVIDED" ]; then
+			echo -n "Password: ";
+			read -n password;
+			nmcli dev wifi connect $ssid password "$password" $@
+		fi
 	fi
 }
 
