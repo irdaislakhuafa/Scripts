945073c8d6b9257d3dacc3c5791659be4e3d1a58
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-06-04 01:44:06 +0700

    use $ACCESS

--- lampp/init.sh
+++ lampp/init.sh
@@ -1,7 +1,7 @@
 export PATH=/opt/lampp/bin:$PATH
 export PATH=/opt/lampp:$PATH
 
-alias lampp='doas lampp'
-alias lstart='doas lampp start'
-alias lstop='doas lampp stop'
-alias lstatus='doas lampp status'
+alias lampp="$ACCESS lampp"
+alias lstart="$ACCESS lampp start"
+alias lstop="$ACCESS lampp stop"
+alias lstatus="$ACCESS lampp status"
