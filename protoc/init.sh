b86ffbad161fb2585d312d1088bb1f9a1e1dd3fa
Irda Islakhu Afa <65246900+irdaislakhuafa@users.noreply.github.com>
2023-10-29 01:44:35 +0700

    added aliases to compile protobuf for golang (#1)

---
+++ protoc/init.sh
@@ -0,0 +1,34 @@
+# golang proto compile
+goprocom() {
+	protoc \
+		--proto_path="$protodir" \
+		--go_out="$protodir" \
+		--go-grpc_out="$protodir" \
+		"$protodir/*.proto"
+}
+
+# procto compile
+procom() {
+	lang="$1"
+	if [ "$protodir" = "" ]; then
+		echo 'variable $protodir is empty'
+		return
+	fi
+
+	if ! [ -d "$protodir" ]; then
+		echo "dir '$protodir' is not found"
+		return
+	fi
+
+	if [ "$lang" = "" ]; then
+		echo "language as args 1 is required"
+		return
+	fi
+
+	if [ "$lang" = "go" ]; then
+		goprocom
+	else
+		echo "language '$lang' is not supported"
+		return
+	fi
+}
